658253fb26f68ea2c732e1834de4e68927782875
jackanderson12 <43770968+jackanderson12@users.noreply.github.com>
2022-01-04 06:06:07 -0500

    Initial Commit
    Initial Commit

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        var roundedNum: Float = 0
+        if sender == heightSlider {
+            roundedNum = Float(round((100 * heightSlider.value) / 100))
+            print(roundedNum)
+        }
+        if sender == weightSlider {
+            roundedNum =
+            Float(round((100 * weightSlider.value) / 100))
+            print(roundedNum)
+        }
+    }
+    
 }
 
