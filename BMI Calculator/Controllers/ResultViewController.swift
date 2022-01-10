fb44282afb29880e22bc16d7ee37ec23cb44ca55
jackanderson12 <43770968+jackanderson12@users.noreply.github.com>
2022-01-10 17:11:54 -0500

    Final Commit

---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Jack Anderson on 1/10/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue : String?
+    
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue!
+    }
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+}
